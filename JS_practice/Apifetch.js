16bd5ad57db01c2cab78f73f52ef29e3cfe188e6
yugal112 <yugal0777@gmail.com>
2025-12-17 19:02:49 +0545

    Update Apifetch.js

--- JS_practice/Apifetch.js
+++ JS_practice/Apifetch.js
@@ -1,24 +1,20 @@
 const URL = "https://dummyjson.com/products";
 
-// helper function
 const fetchData = async (endpoint = "") => {
   const res = await fetch(`${URL}${endpoint}`);
   return res.json();
 };
 
-// Single Product
 const getSingleProduct = async () => {
   const data = await fetchData("/1");
   console.log(data);
 };
 
-// All Products
 const getAllProduct = async () => {
   const data = await fetchData();
   console.log(data);
 };
 
-// All Product Images
 const getAllProductImage = async () => {
   const { products } = await fetchData();
   products.forEach(product => {
@@ -26,13 +22,11 @@ const getAllProductImage = async () => {
   });
 };
 
-// Product Stock Check
 const productStockCheck = async (id) => {
   const { stock } = await fetchData(`/${id}`);
   console.log(stock);
 };
 
-// Total Product Available
 const totalProductAvailable = async () => {
   const { products } = await fetchData();
   const count = products.filter(
@@ -42,7 +36,6 @@ const totalProductAvailable = async () => {
   console.log(count);
 };
 
-// function calls
 getAllProduct();
 getSingleProduct();
 getAllProductImage();
